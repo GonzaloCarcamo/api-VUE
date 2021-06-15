f215bfb87dba6455561bf6b264e8bad257ba78d6
goncmodev <gon.cmo.dev@gmail.com>
2021-06-15 02:43:35 +0100

    api with axios y vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <div>
+                <h1>API - jsonplaceholder</h1>
+                <table>
+                        <tbody>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <tr v-for="todo in todos" :key="todo.id">
+                                        <td>{{todo.id }}</td>
+                                        <td>{{todo.name }}</td>
+                                        <td>{{todo.username }}</td>
+                                        <td>{{todo.email }}</td>
+                                </tr>
+                        </tbody>
+                </table>
+        </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+        data(){
+                return {
+                        todos:null
+                }
+        },
+
+        mounted(){
+                console.log('hola desde mounted');
+                this.getTodos();
+        },
+
+        methods: {
+                getTodos(){
+                        console.log('get todos');
+                        axios.get('https://jsonplaceholder.typicode.com/users')
+                        .then( response => {
+                                this.todos = response.data;
+                        })
+                        .catch( e => console.log(e))
+                }
+        }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+        *{
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                text-align: justify;
+        }
+        table{
+                width: 600px;
+        }
+
+        th{
+                background: rgb(220, 220, 255);
+                font-weight: 800;
+                text-align: center;
+        }
+
+        td{
+                border: 1px dashed rgb(163, 159, 159);
+        }
 </style>
